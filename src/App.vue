17cb17de423835754e53f9fb74f92d357966e8a7
dg <707170913@qq.com>
2018-10-07 19:39:46 +0800

    add HomeHeader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
     <router-view/>     <!--显示的是当前路由地址所对应的内容-->
   </div>
 </template>
--- src/Pages/Home/home.vue
+++ src/Pages/Home/home.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <div class="home">home</div>
-    </div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
